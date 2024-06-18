4e29ca0251ac844b34e2944308702c3fd2551dcb
Phuong175 <138676968+Phuong175@users.noreply.github.com>
2024-06-18 15:54:01 +0700

    up_login

--- src/components/screens/users/Login.js
+++ src/components/screens/users/Login.js
@@ -1,14 +1,207 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import Icons from 'src/components/icons/Icon'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ToastAndroid
+} from 'react-native'
 import React from 'react'
 
-const Login = () => {
+const Login = ({ navigation: { goBack } }) => {
   return (
-    <View>
-      <Text style={{textAlign: 'center'}}>Login</Text>
+    <View
+      style={{
+        flexDirection: 'column',
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#F9F9F9'
+      }}
+    >
+      <View style={styles.view_search}>
+        <TouchableOpacity onPress={() => goBack()}>
+          <Icons.Ionicons name={'chevron-back'} size={24} />
+        </TouchableOpacity>
+      </View>
+      <View style={{ margin: 10 }}>
+        <Text
+          style={{
+            width: 94,
+            height: 34,
+            fontSize: 34,
+            fontWeight: '700',
+            fontStyle: 'normal',
+            lineHeight: 34,
+            color: '#222222'
+          }}
+        >
+          Login
+        </Text>
+
+        <View
+          style={{
+            marginHorizontal: 16,
+            marginTop: 73,
+            width: 343,
+            height: 64,
+            borderRadius: 4,
+            backgroundColor: '#FFFFFF',
+            shadowColor: 'rgba(0, 0, 0, 0.05)',
+            shadowOffset: {
+              width: 0,
+              height: 1
+            },
+            shadowRadius: 8,
+            shadowOpacity: 1
+          }}
+        >
+          <TextInput placeholder="Email" />
+        </View>
+        <View
+          style={{
+            marginHorizontal: 16,
+            marginTop: 8,
+            width: 343,
+            height: 64,
+            borderRadius: 4,
+            backgroundColor: '#FFFFFF',
+            shadowColor: 'rgba(0, 0, 0, 0.05)',
+            shadowOffset: {
+              width: 0,
+              height: 1
+            },
+            shadowRadius: 8,
+            shadowOpacity: 1
+          }}
+        >
+          <TextInput placeholder="Password" />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.txtforgot}>Forgot your password? </Text>
+          <Image
+            style={{ width: '15', height: 10}}
+            source={require('@assets/Vector.png')}
+          />
+        </View>
+        
+
+        <View style={styles.btnLogin}>
+          <TouchableOpacity style={{ alignItems: 'center' }}>
+            <Text style={styles.txtbtn}>LOGIN</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.txtSignWith}> Or sign up with social account</Text>
+        <View style={{flexDirection: 'row'}}>
+        <View
+          style={{
+            marginLeft:89,
+            marginTop: 10,
+            width: 80,
+            height: 64,
+            borderRadius: 24,
+            backgroundColor: '#FFFFFF',
+            shadowColor: 'rgba(0, 0, 0, 0.05)',
+            shadowOffset: {
+              width: 0,
+              height: 1
+            },
+            shadowRadius: 8,
+            shadowOpacity: 1
+          }}
+        >
+          <Image
+            style={{ width: '23.495864868164062', height: 50, position: 'relative' }}
+            source={require('@assets/gg_logo.png')}
+          />
+        </View>
+        <View
+          style={{
+            marginLeft: 40,
+            marginTop: 5,
+            width: 65,
+            height: 64,
+            borderRadius: 24,
+            backgroundColor: '#FFFFFF',
+            shadowColor: 'rgba(0, 0, 0, 0.05)',
+            shadowOffset: {
+              width: 0,
+              height: 1
+            },
+            shadowRadius: 8,
+            shadowOpacity: 1
+          }}
+        >
+          <Image
+            style={{ width: '23.495864868164062', height: 60, position: 'relative' }}
+            source={require('@assets/fb_logo.png')}
+          />
+        </View>
+        </View>
+        
+      </View>
     </View>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  txtSignWith: {
+    marginLeft: 86,
+    marginTop: 182,
+    width: 200,
+    height: 20,
+    fontFamily: 'Metropolis',
+    fontSize: 14,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    lineHeight: 20,
+    textAlign: 'center',
+    color: '#222222'
+  },
+  view_search: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 8,
+    marginTop: 44
+  },
+  txtforgot: {
+    marginLeft: 174,
+    width: 158,
+    height: 20,
+    fontFamily: 'Metropolis',
+    fontSize: 14,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    lineHeight: 20,
+    textAlign: 'right',
+    color: '#222222'
+  },
+  btnLogin: {
+    marginTop: 32,
+    width: 343,
+    height: 48,
+    borderRadius: 25,
+    backgroundColor: '#DB3022',
+    shadowColor: 'rgba(211, 38, 38, 0.25)',
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 8,
+    shadowOpacity: 1
+  },
+  txtbtn: {
+    marginVertical: 14,
+    width: 44,
+    height: 20,
+    fontFamily: 'Metropolis',
+    fontSize: 14,
+    fontWeight: '500',
+    fontStyle: 'normal',
+    lineHeight: 20,
+    color: '#FFFFFF'
+  }
+})
