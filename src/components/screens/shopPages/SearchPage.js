540ba8ac910463b7a53f73b28c03fd43c9c75c96
anhaylientuc <thang3051998no1@gmail.com>
2024-08-20 19:42:31 +0700

    ok

--- src/components/screens/shopPages/SearchPage.js
+++ src/components/screens/shopPages/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import {
   Animated,
   Dimensions,
@@ -7,24 +7,64 @@ import {
   TextInput,
   TouchableOpacity,
   View
-} from 'react-native'
-import Colors from 'src/constants/Colors'
-import MyText from 'src/constants/FontFamily'
-import Icons from '../../icons/Icon'
-const windowWith = Dimensions.get('window').width
-const windowHeight = Dimensions.get('window').height
-const SearchPage = props => {
-  const { navigation } = props
+} from 'react-native';
+import Colors from 'src/constants/Colors';
+import MyText from 'src/constants/FontFamily';
+import Icons from '../../icons/Icon';
+import NewHTTP from 'src/utils/http/NewHTTP';
+import qs from 'qs';
 
-  const position = new Animated.ValueXY({ x: 0, y: 500 })
-  Animated.timing(position, {
-    toValue: { x: 0, y: 0 },
-    duration: 500,
-    useNativeDriver: true
-  }).start()
+const windowWith = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const SearchPage = props => {
+  const { navigation } = props;
+  const [keyword, setkeyword] = useState('');
+  const [debouncedKeyword, setdebouncedKeyword] = useState(keyword);
+  const [suggest, setsuggest] = useState([]);
+
+  const position = new Animated.ValueXY({ x: 0, y: 0 });
+
+  // Chỉ thực hiện hoạt ảnh khi màn hình được hiển thị lần đầu tiên
   useEffect(() => {
-    navigation.getParent().setOptions({ tabBarStyle: { display: 'none' } })
-  }, [])
+    Animated.timing(position, {
+      toValue: { x: 0, y: 0 },
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  }, []); // [] đảm bảo rằng hoạt ảnh chỉ chạy một lần khi màn hình được render lần đầu
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      console.log(keyword);
+      setdebouncedKeyword(keyword);
+    }, 500);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [keyword]);
+
+  useEffect(() => {
+    if (debouncedKeyword) {
+      console.log(debouncedKeyword);
+      const fetchData = async () => {
+        const query = {};
+        query.keyword = debouncedKeyword;
+        const queryString = qs.stringify(query);
+        const response = await NewHTTP.search(queryString);
+        setsuggest(response);
+      };
+      fetchData();
+    } else {
+      setsuggest([]);
+    }
+  }, [debouncedKeyword]);
+
+  useEffect(() => {
+    navigation.getParent().setOptions({ tabBarStyle: { display: 'none' } });
+  }, []);
+
   const setBottomBar = () => {
     navigation.getParent().setOptions({
       tabBarStyle: {
@@ -32,10 +72,10 @@ const SearchPage = props => {
         bottom: 0,
         paddingVertical: 8,
         height: 54
-        // position: 'absolute'
       }
-    })
-  }
+    });
+  };
+
   return (
     <Animated.View
       style={{
@@ -67,7 +107,8 @@ const SearchPage = props => {
             color: Colors.black,
             width: windowWith - 100
           }}
-        ></TextInput>
+          onChangeText={text => setkeyword(text)}
+        />
       </View>
       <View
         style={{
@@ -88,22 +129,27 @@ const SearchPage = props => {
           </Text>
           <Text style={{ color: Colors.black, fontFamily: 'Montserrat-SemiBold' }}>Xóa</Text>
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: 32
-          }}
-        >
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icons.EvilIcons name="clock" size={32} />
-            <MyText fontFamily="Montserrat-SemiBold" style={{ marginStart: 16 }}>
-              đầm
-            </MyText>
-          </View>
-          <Icons.AntDesign name="arrowright" size={24} />
-        </View>
+        {
+          suggest.map((item, index) => (
+            <View
+              key={index}
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                marginTop: 32
+              }}
+            >
+              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Icons.EvilIcons name="search" size={32} />
+                <MyText fontFamily="Montserrat-SemiBold" style={{ marginStart: 16 }}>
+                  {item}
+                </MyText>
+              </View>
+              <Icons.AntDesign name="arrowright" size={24} />
+            </View>
+          ))
+        }
       </View>
       <View style={{ position: 'absolute', bottom: 16 }}>
         <View
@@ -129,9 +175,9 @@ const SearchPage = props => {
         </View>
       </View>
     </Animated.View>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
